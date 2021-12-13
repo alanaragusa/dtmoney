1ce4af7c80a2bb27dc3ff3c899588efcc8826c7d
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-12-13 18:02:53 -0300

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import styled from 'styled-components';
-
-// styled components - criar um componente estilizado //
-// vantagem styled components - estilização fica dentro do escopo do componente //  
-const Title = styled.h1 `
-  font-size: 64px;
-  color: #8257e6;
-`
-
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
     </div>
   );
 }
