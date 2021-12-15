c3797e14e021bcf737ba46235b5e0fb85c4c6f83
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-12-15 19:45:19 -0300

    modal structure ok

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -7,11 +8,19 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return(
-        <Modal 
+      <Modal 
         isOpen={isOpen}
         onRequestClose={onRequestClose}
       >
-        <h2>Cadastrar transação</h2>
+
+        <Container>
+          <h2>Cadastrar transação</h2>
+          <input placeholder="Título" />
+          <input type="number" placeholder="Valor" />
+          <input placeholder="Categoria" />
+          <button type="submit">Cadastrar</button>
+        </Container>
+        
       </Modal>
     );
 }
