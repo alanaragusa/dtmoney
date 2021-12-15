764d46118d5c616fd743327fe4f967c109a70598
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-12-15 19:13:13 -0300

    modal component in app.tsx file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
-    // modal //
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false); // modal fechado // 
-    
-    // handle - usuario vai clicar em alguma coisa ou executar algum comando //
-    function handleOpenNewTransactionModal () {
-        setIsNewTransactionModalOpen(true);
-    }
+// propriedades que o Header vai receber // 
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
 
-    function handleCloseNewTransactionModal () {
-        setIsNewTransactionModalOpen(false);
-    }
-
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button" onClick={handleOpenNewTransactionModal} >
+                <button type="button" onClick={onOpenNewTransactionModal} >
                     Nova transação
                 </button>
             </Content>
-            <Modal 
-            isOpen={isNewTransactionModalOpen}
-            onRequestClose={handleCloseNewTransactionModal}
-            >
-                <h2>Cadastrar transação</h2>
-            </Modal>
+            
         </Container>
     )
 }
